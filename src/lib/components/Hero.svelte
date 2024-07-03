d5ecf12c7b2c24786585652b8b2889920a3ab986
Nathan G <ngarrow@webstacks.com>
2024-07-03 12:04:06 -0500

    feat: logo click event

--- src/lib/components/Hero.svelte
+++ src/lib/components/Hero.svelte
@@ -1,47 +1,81 @@
 <script>
 	import Icon from 'svelte-awesome/components/Icon.svelte';
 	import { faReact } from '@awesome.me/kit-7afeb9cb5d/icons/classic/brands';
+	import { cva } from 'cva';
+
+	/**
+	 * @typedef {'default' | 'svelte' | 'react' | 'gatsby'} ActiveGradient
+	 * @type {ActiveGradient}
+	 */
+	let activeGradient = $state('default');
+
+	const heroClass = cva('center-content relative min-h-svh pt-32 pb-16 md:min-h-[80svh] lg:pt-24 lg:pb-20 xl:pt-32', {
+		variants: {
+			gradient: {
+				default: 'hero-gradient-default',
+				svelte: 'hero-gradient-svelte',
+				react: 'hero-gradient-react',
+				gatsby: 'hero-gradient-gatsby',
+			},
+		},
+
+		defaultVariants: {
+			gradient: 'default',
+		},
+	});
 </script>
 
-<section
-	class="hero-gradient center-content relative min-h-svh pt-32 pb-16 md:min-h-[80svh] lg:pt-24 lg:pb-20 xl:pt-32"
->
+<section class={heroClass({ gradient: activeGradient })}>
 	<div class="absolute inset-0 z-[2] size-full">
-		<div
+		<button
+			type="button"
+			onclick={() => (activeGradient = 'react')}
 			class="icon shadow-blue/20 text-blue hover:text-blue-light top-[5%] left-[25%] size-20 rotate-5 shadow-xl md:top-[10%] lg:size-24"
 		>
 			<Icon data={faReact} class="size-full" />
-		</div>
-		<div
+		</button>
+		<button
+			type="button"
+			onclick={() => (activeGradient = 'svelte')}
 			class="icon shadow-orange/20 text-orange hover:text-orange-light top-[25%] left-[7%] size-20 -rotate-2 shadow-xl md:top-[35%] lg:size-36"
 		>
 			<i class="fa-kit fa-svelte !size-full"></i>
-		</div>
-		<div
+		</button>
+		<button
+			type="button"
+			onclick={() => (activeGradient = 'default')}
 			class="icon shadow-offwhite/20 text-offwhite top-[80%] left-[12%] size-20 -rotate-12 shadow-xl hover:text-white md:top-[70%] lg:size-20"
 		>
 			<i class="fa-kit fa-nextjs !size-full"></i>
-		</div>
-		<div
+		</button>
+		<button
+			type="button"
+			onclick={() => (activeGradient = 'gatsby')}
 			class="icon shadow-purple/20 text-purple hover:text-purple-light top-[15%] right-[42%] size-14 -rotate-3 opacity-65 shadow-xl md:top-[6%]"
 		>
 			<i class="fa-kit fa-gatsby !size-full"></i>
-		</div>
-		<div
+		</button>
+		<button
+			type="button"
+			onclick={() => (activeGradient = 'svelte')}
 			class="icon shadow-orange/20 text-orange hover:text-orange-light top-[75%] right-[26%] size-20 rotate-12 shadow-xl md:top-[13%] lg:size-24"
 		>
 			<i class="fa-kit fa-svelte !size-full"></i>
-		</div>
-		<div
+		</button>
+		<button
+			type="button"
+			onclick={() => (activeGradient = 'default')}
 			class="icon text-offwhite top-[10%] right-[10%] size-20 -rotate-6 shadow-xl shadow-white/20 hover:text-white md:top-[30%] lg:size-40"
 		>
 			<i class="fa-kit fa-nextjs !size-full"></i>
-		</div>
-		<div
+		</button>
+		<button
+			type="button"
+			onclick={() => (activeGradient = 'react')}
 			class="icon shadow-blue/20 text-blue hover:text-blue-light top-[85%] right-[10%] size-20 rotate-8 shadow-xl md:top-[65%] lg:size-24"
 		>
 			<Icon data={faReact} class="size-full" />
-		</div>
+		</button>
 	</div>
 	<div class="relative z-10 mx-auto max-w-6xl px-3 text-center">
 		<div class="space-y-6">
@@ -55,12 +89,31 @@
 </section>
 
 <style>
-	.hero-gradient {
+	.hero-gradient-default {
 		background-image: linear-gradient(
 			to bottom,
 			var(--color-card-background-0),
 			var(--color-card-background) 90%,
 			var(--color-card-background-0)
+		);
+	}
+	.hero-gradient-svelte {
+		background-image: linear-gradient(
+			to bottom,
+			var(--color-orange-0),
+			var(--color-orange-40) 90%,
+			var(--color-orange-0)
+		);
+	}
+	.hero-gradient-react {
+		background-image: linear-gradient(to bottom, var(--color-blue-0), var(--color-blue-40) 90%, var(--color-blue-0));
+	}
+	.hero-gradient-gatsby {
+		background-image: linear-gradient(
+			to bottom,
+			var(--color-purple-0),
+			var(--color-purple-40) 90%,
+			var(--color-purple-0)
 		);
 	}
 
@@ -70,6 +123,7 @@
 		border-radius: 8px;
 		padding: 16px;
 		background-color: var(--color-card-background);
+		cursor: pointer;
 		opacity: 0.3;
 
 		@media (width >= 40rem) {
