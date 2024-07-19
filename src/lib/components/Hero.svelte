fc0f10e76fe487d24461e543a766a2e37870fdb3
Nathan G <ngarrow@webstacks.com>
2024-07-19 16:14:19 -0500

    chore: refactor slot -> @render

--- src/lib/components/Hero.svelte
+++ src/lib/components/Hero.svelte
@@ -1,13 +1,18 @@
-<script>
+<script lang="ts">
 	import Icon from 'svelte-awesome/components/Icon.svelte';
 	import { faReact } from '@awesome.me/kit-7afeb9cb5d/icons/classic/brands';
+
 	import { cva } from 'cva';
 
-	/**
-	 * @typedef {'default' | 'svelte' | 'react' | 'gatsby'} ActiveGradient
-	 * @type {ActiveGradient}
-	 */
-	let activeGradient = $state('default');
+	type Props = {
+		eyebrowText?: string;
+		titleTop?: string;
+		titleBottom?: string;
+		descriptionText?: string;
+		children: any;
+	};
+	const { eyebrowText, titleTop, titleBottom, descriptionText, children }: Props = $props();
+	let activeGradient: 'default' | 'svelte' | 'react' | 'gatsby' = $state('default');
 
 	const heroClass = cva('center-content relative min-h-svh pt-32 pb-16 md:min-h-[95svh] lg:pt-24 lg:pb-20 xl:pt-32', {
 		variants: {
@@ -25,63 +30,87 @@
 	});
 </script>
 
+{#snippet eyebrow(eyebrowText)}
+	<span slot="eyebrow" class="font-code text-slate text-sm tracking-widest sm:text-lg xl:text-xl">{eyebrowText}</span>
+{/snippet}
+
+{#snippet title(titleTextTop, titleTextBottom)}
+	<h1 slot="title" class="flex flex-col font-medium text-transparent">
+		<span class="title-span gradient-text text-2xl sm:text-4xl">{titleTextTop}</span>
+		<span class="title-span gradient-text relative text-4xl sm:text-5xl lg:-top-3 xl:text-7xl">
+			{titleTextBottom}
+		</span>
+	</h1>
+{/snippet}
+
+{#snippet description(descriptionText)}
+	<div class="text-md max-w-2xl leading-tight xl:max-w-4xl xl:text-lg">
+		{@html descriptionText}
+	</div>
+{/snippet}
+
 <section class={heroClass({ gradient: activeGradient })}>
 	<div class="relative z-10 mx-auto max-w-6xl px-3 text-center">
 		<div class="space-y-6">
-			<slot name="eyebrow" />
+			{@render eyebrow(eyebrowText)}
+			<!-- <slot name="eyebrow" />
 			<slot name="title" />
-			<slot name="description" />
+			<slot name="description" /> -->
+			{@render title(titleTop, titleBottom)}
 
-			<slot />
+			{@render description(descriptionText)}
+
+			<!-- <slot /> -->
+			{@render children()}
 		</div>
 	</div>
 	<div class="absolute inset-0 z-[2] size-full" aria-hidden="true">
 		<button
 			type="button"
 			onclick={() => (activeGradient = 'react')}
-			class="icon shadow-blue/20 text-blue hover:text-blue-light top-[8%] left-[20%] size-16 rotate-5 shadow-xl md:top-[10%] lg:size-24"
+			class="icon shadow-blue/20 text-blue hover:text-blue-light top-[8%] left-[20%] size-16 rotate-5 shadow-xl md:top-[10%] lg:size-[6svw]"
 		>
 			<Icon data={faReact} class="size-full" />
 		</button>
 		<button
 			type="button"
 			onclick={() => (activeGradient = 'svelte')}
-			class="icon shadow-orange/20 text-orange hover:text-orange-light top-[20%] left-[5%] size-16 -rotate-2 shadow-xl md:top-[35%] lg:size-28"
+			class="icon shadow-orange/20 text-orange hover:text-orange-light top-[20%] left-[5%] size-16 -rotate-2 shadow-xl md:top-[35%] lg:size-[9svw]"
 		>
 			<i class="fa-kit fa-svelte !size-full"></i>
 		</button>
 		<button
 			type="button"
 			onclick={() => (activeGradient = 'default')}
-			class="icon shadow-offwhite/20 text-offwhite top-[85%] left-[10%] size-16 -rotate-12 shadow-xl hover:text-white md:top-[75%] lg:size-20"
+			class="icon shadow-offwhite/20 text-offwhite top-[85%] left-[10%] size-16 -rotate-12 shadow-xl hover:text-white md:top-[75%] lg:size-[6svw]"
 		>
 			<i class="fa-kit fa-nextjs !size-full"></i>
 		</button>
 		<button
 			type="button"
 			onclick={() => (activeGradient = 'gatsby')}
-			class="icon shadow-purple/20 text-purple hover:text-purple-light top-[15%] right-[30%] size-14 -rotate-3 opacity-65 shadow-xl md:top-[6%]"
+			class="icon shadow-purple/20 text-purple hover:text-purple-light top-[15%] right-[30%] size-14 -rotate-3 opacity-65 shadow-xl md:top-[6%] lg:size-[4svw]"
 		>
 			<i class="fa-kit fa-gatsby !size-full"></i>
 		</button>
 		<button
 			type="button"
 			onclick={() => (activeGradient = 'svelte')}
-			class="icon shadow-orange/20 text-orange hover:text-orange-light top-[83%] right-[38%] size-16 rotate-12 shadow-xl md:top-[13%] md:right-[18%] lg:size-20"
+			class="icon shadow-orange/20 text-orange hover:text-orange-light top-[83%] right-[38%] size-16 rotate-12 shadow-xl md:top-[13%] md:right-[18%] lg:size-[6svw]"
 		>
 			<i class="fa-kit fa-svelte !size-full"></i>
 		</button>
 		<button
 			type="button"
 			onclick={() => (activeGradient = 'default')}
-			class="icon text-offwhite top-[12%] right-[8%] size-16 -rotate-6 shadow-xl shadow-white/20 hover:text-white md:top-[30%] lg:size-24"
+			class="icon text-offwhite top-[12%] right-[8%] size-16 -rotate-6 shadow-xl shadow-white/20 hover:text-white md:top-[30%] lg:size-[6svw]"
 		>
 			<i class="fa-kit fa-nextjs !size-full"></i>
 		</button>
 		<button
 			type="button"
 			onclick={() => (activeGradient = 'react')}
-			class="icon shadow-blue/20 text-blue hover:text-blue-light top-[90%] right-[10%] size-16 rotate-8 shadow-xl md:top-[70%] lg:size-20"
+			class="icon shadow-blue/20 text-blue hover:text-blue-light top-[90%] right-[10%] size-16 rotate-8 shadow-xl md:top-[70%] lg:size-[7svw]"
 		>
 			<Icon data={faReact} class="size-full" />
 		</button>
@@ -133,5 +162,18 @@
 		@media (width >= 64rem) {
 			opacity: 1;
 		}
+	}
+
+	.title-span {
+		position: relative;
+		display: inline-block;
+	}
+	.gradient-text {
+		background-clip: text;
+		-webkit-background-clip: text;
+		-webkit-text-fill-color: transparent;
+
+		background-image: linear-gradient(to bottom, var(--color-offwhite) 10%, var(--color-white) 35%, var(--color-slate));
+		line-height: 1.15;
 	}
 </style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -16,23 +16,17 @@
 	let { jobs = [], testimonials = [] }: Props = data;
 </script>
 
-<Hero>
-	<span slot="eyebrow" class="font-code text-slate text-sm tracking-widest sm:text-lg xl:text-xl">Nate Garrow</span>
-	<h1 slot="title" class="flex flex-col font-medium text-transparent">
-		<span class="title-span gradient-text text-2xl sm:text-4xl">Enhancing the Web</span>
-		<span class="title-span gradient-text relative text-4xl sm:text-5xl lg:-top-3 xl:text-7xl">
-			One Line at a Time
-		</span>
-	</h1>
-	<div slot="description" class="text-md max-w-2xl leading-tight xl:max-w-4xl xl:text-lg">
-		<p>
+<Hero
+	eyebrowText="Nate Garrow"
+	titleTop="Enhancing the Web"
+	titleBottom="One Line at a Time"
+	descriptionText={`<p>
 			As a <b class="font-medium text-white">Frontend Web Developer</b> driven by a passion for innovation, I specialize
 			in crafting seamless web solutions that elevate user experiences. With meticulous attention to detail and expertise
 			in cutting-edge technologies like React, each line of code contributes to a more dynamic and user-friendly digital
 			world.
-		</p>
-	</div>
-
+		</p>`}
+>
 	<div class="mt-12 mx-auto w-fit">
 		<a
 			href="#success-stories"
@@ -42,15 +36,13 @@
 	</div>
 </Hero>
 
-<Heading>
-	<h2 slot="heading" class="text-3xl font-semibold tracking-widest text-white xl:text-4xl">The Web is Evolving</h2>
-	<div slot="subheading" class="mx-auto max-w-3xl text-lg xl:text-xl">
-		<p>
+<Heading
+	headingText="The Web is Evolving"
+	subheadingText={`<p>
 			Web development is constantly evolving, and so is my portfolio. I continuously rebuild and update it to stay ahead
 			with the latest technologies and trends, ensuring cutting-edge solutions for every project.
-		</p>
-	</div>
-</Heading>
+		</p>`}
+/>
 
 <Portfolio />
 
@@ -63,18 +55,3 @@
 {/if}
 
 <Footer />
-
-<style>
-	.title-span {
-		position: relative;
-		display: inline-block;
-	}
-	.gradient-text {
-		background-clip: text;
-		-webkit-background-clip: text;
-		-webkit-text-fill-color: transparent;
-
-		background-image: linear-gradient(to bottom, var(--color-offwhite) 10%, var(--color-white) 35%, var(--color-slate));
-		line-height: 1.15;
-	}
-</style>
